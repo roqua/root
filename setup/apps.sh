49cf9b4c97d0aed40bb8ca451a627f72e7d8f479
Marten Veldthuis <marten@veldthuis.com>
2014-08-04 09:54:13 +0200

    Reset database

--- setup/apps.sh
+++ setup/apps.sh
@@ -6,8 +6,11 @@ function renderTemplate() {
 
 pushd $ROOT > /dev/null
   app="rom"
-  renderTemplate ./config/mysql.yml > apps/roqua/config/database.yml
-  renderTemplate ./config/mongoid.yml > apps/roqua/config/mongoid.yml
+  renderTemplate ./config/mysql.yml > apps/rom/config/database.yml
+  renderTemplate ./config/mongoid.yml > apps/rom/config/mongoid.yml
+  pushd $ROOT/apps/rom > /dev/null
+    bundle exec rake db:reset ORGANIZATION=demo
+  popd > /dev/null
 
   app="core"
   renderTemplate ./config/postgresql.yml > apps/core/config/database.yml
